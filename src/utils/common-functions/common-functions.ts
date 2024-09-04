aedcb11464af9a6f16e7056563341279979a6a87
pinanklakhani <pinank.lakhani@smartsensesolutions.com>
2024-09-04 18:19:26 +0530

    fix: software, data resource development

--- src/utils/common-functions/common-functions.ts
+++ src/utils/common-functions/common-functions.ts
@@ -186,6 +186,7 @@ class Utils {
 		rsaAlso: string,
 		x5uURL: string
 	) {
+		console.log('====privateKey===>', privateKey)
 		const canonizedSD = await this.normalize(
 			jsonld,
 			// eslint-disable-next-line
